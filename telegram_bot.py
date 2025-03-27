9100b6d6369933c2dd55a7e88eb7cffaf200cd08
Nikolai124 <g4728281919@gmail.com>
2025-03-27 19:59:23 +0300

    publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,3 +1,4 @@
 import telegram
 bot = telegram.Bot(token='7710345710:AAHjeBShi6f-4D0idj39aW5_uWRJQF9UzRc')
 bot.send_message(chat_id='@erg3i', text="I'm sorry Dave I'm afraid I can't do that.")
+bot.send_document(chat_id='@erg3i', document=open('images/EPIC_0.png', 'rb'))
