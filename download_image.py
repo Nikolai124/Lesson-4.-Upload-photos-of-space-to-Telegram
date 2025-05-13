ef3ff44da9abca7f3843fb23155f50d850895dd6
Nikolai124 <g4728281919@gmail.com>
2025-05-13 19:18:20 +0300

    использую None в download_image.py

--- download_image.py
+++ download_image.py
@@ -1,6 +1,6 @@
 import requests
 
-def download_image (url, file_path, params = "none"):
+def download_image (url, file_path, params = None):
     response = requests.get(url, params = params)
     response.raise_for_status() 
     with open(file_path, 'wb') as file: 
