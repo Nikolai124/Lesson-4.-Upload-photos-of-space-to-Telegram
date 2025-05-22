2d159293983dc12571c3005a6c0656bae4a0fb87
Nikolai124 <g4728281919@gmail.com>
2025-05-22 19:56:24 +0300

    разбил функцию на части

--- receiving_nasa_apod_images.py
+++ receiving_nasa_apod_images.py
@@ -7,10 +7,20 @@ from pathlib import Path
 from download_image import download_image
 
 
+def get_nasa_apod_images(nasa_image, image_number, path):
+    link = nasa_image['url'] 
+    link_expansion = receive_expansion(link) 
+    filename = f"nasa_apod_{image_number}{link_expansion}"
+    file_path = os.path.join(path, filename)
+    download_image(link, file_path) 
+
+
+
 def receive_expansion(link): 
     link_path = urlparse(link).path 
     expansion = os.path.splitext(link_path)[1] 
     return expansion 
+
 
 def get_nasa_apod(count, api_key):
     url = "https://api.nasa.gov/planetary/apod"
@@ -21,6 +31,7 @@ def get_nasa_apod(count, api_key):
     response = requests.get(url, params=params) 
     response.raise_for_status()
     return response.json()
+
 
 def main(): 
     load_dotenv()
@@ -35,12 +46,8 @@ def main():
     nasa_apod_images = get_nasa_apod(args.count, api_key)
     for image_number, nasa_image in enumerate(nasa_apod_images):
         if nasa_image['media_type'] == 'image':
-            link = nasa_image['url'] 
-            link_expansion = receive_expansion(link) 
-            filename = f"nasa_apod_{image_number}{link_expansion}"
-            file_path = os.path.join(args.path, filename)
-            download_image(link, file_path) 
-    print(nasa_image)
+            get_nasa_apod_images(nasa_image, image_number, args.path)
+
 
 if __name__ == "__main__":
     main()
